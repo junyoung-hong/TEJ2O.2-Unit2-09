006ff5841151f85f51725cf9383762a71e4107bf
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-03-27 12:57:44 -0400

    CODE

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: Mar 2026
+ * This program do rock, paper , sissor.
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+let score: number = 0
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// shake the microbit
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 1)
+    basic.clearScreen()
+
+    // if randomNumber was 0
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    // if randomNumber was 1
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+    //if randomNumber was 2
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+
+    // pasue and show you are ready again
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+})
+
+// press A
+    input.onButtonPressed(Button.A, function() {
+        score += 1
+    })
+
+// press B
+    input.onButtonPressed(Button.B, function() {
+        basic.clearScreen()
+        basic.showString(score.toString())    
+        
+    })
+   
